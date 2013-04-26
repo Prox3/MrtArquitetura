a3c076fdd15f2b51f16828b41f78aff23902d17f
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-04-25 21:33:37 -0300

    Alteração Sidebar

--- wp-content/themes/mrtArquiteturaTheme/sidebar.php
+++ wp-content/themes/mrtArquiteturaTheme/sidebar.php
@@ -1,6 +1,20 @@
 <div id="sidebar">
 	<div id="contentSearch" style="display:none;clear:both;">
     </div>
+    <a href="<?php get_site_url(); ?>/novo/category/marcenaria/">
+    	<div style="margin-bottom: 10px;">
+        	<div class="tituloSidebar centralizarVertical clearDiv">
+            	Marcenaria
+        	</div>
+    	</div>
+    </a>
+    <a href="<?php get_site_url(); ?>/novo/category/regularizacao-de-imoveis/">
+    	<div style="margin-bottom: 10px;">
+        	<div class="tituloSidebar centralizarVertical clearDiv" style="font-size: 18px;">
+            	Regularização de imóveis
+        	</div>
+    	</div>
+    </a>
     <div>
         <div class="tituloSidebar centralizarVertical clearDiv">
             Categorias
@@ -14,9 +28,11 @@
 				$("#sidebar ul, #sidebar ul li, #sidebar ul li a").removeAttr("class").removeAttr("id");
 				$.map($("#sidebar ul li a"), function(t,index){
 					$(t).attr("class", "linkSidebar upperCase");
-					var id = removeAcento($(t).text().split(" ")[0]);
-					if(id.toLowerCase() == "portfolio")
+					var id = $(t).text();//removeAcento($(t).text().split(" ")[0]);
+					if(id.toLowerCase() == "portfolio" || id.toLowerCase() == "marcenaria" || id.toLowerCase() == "regularizacao de imoveis"){
+						alert(id.toLowerCase());
 						$($(t).parent()[0]).remove();
+					}
 				});
 			</script>
 			');
